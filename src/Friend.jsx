5272da55211c7713b4c20232c40f0b59deaf63ec
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-03-10 20:27:02 +0600

    Updated

--- src/Friend.jsx
+++ src/Friend.jsx
@@ -2,13 +2,14 @@
 
 // Display object data.
 export default function Friend({ friend }) {
+    const { name, username, email } = friend;
     return (
         <div>
 
             <div className="box">
-                <p>Name: {friend.name}</p>
-                <p>User Name: {friend.username}</p>
-                <p>Email: {friend.email}</p>
+                <p>Name: {name}</p>
+                <p>User Name: {username}</p>
+                <p>Email: {email}</p>
 
             </div>
         </div>
